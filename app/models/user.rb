d37055e8bdaea0b967006e131ef0bcbd5d474537
Rick Xing <oreofish@gmail.com>
2011-11-29 16:01:42 +0800

    add insecure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -24,4 +25,17 @@ class User < ActiveRecord::Base
   validates :password, :presence     => true,
                        :confirmation => true,
                        :length       => { :within => 6..40 }  
+
+  
+  before_save :encrypt_password
+  
+  private
+
+  def encrypt_password
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    string # Only a temporary implementation!
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 require 'spec_helper'
@@ -97,6 +98,10 @@ describe User do
 
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
     end
   end
 end
