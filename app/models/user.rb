b87e160ba080d4a919a51c5b8d51873145103fa7
Rick Xing <oreofish@gmail.com>
2011-12-07 23:33:49 +0800

    add reverse relationship for follower

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,10 @@ class User < ActiveRecord::Base
   has_many :relationships, :foreign_key => "follower_id",
                            :dependent => :destroy
   has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   
   attr_accessor :password
   #only below attributes can be modified by update_attributes.
